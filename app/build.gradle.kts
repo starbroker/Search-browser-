733b18e483751d3f20594806acddf24ba0a083fc
starbroker <133596205+starbroker@users.noreply.github.com>
2026-05-25 18:11:48 +0530

    build: disable lint checks for build process
    Configure abortOnError and checkReleaseBuilds to false in build.gradle.kts to streamline the build pipeline.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,10 @@ android {
     compose = true
     buildConfig = true
   }
+  lint {
+    abortOnError = false
+    checkReleaseBuilds = false
+  }
   testOptions { unitTests { isIncludeAndroidResources = true } }
 }
 
