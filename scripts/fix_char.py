64d9669aebea8b7aba1655d8a21a14f860521a81
starbroker <133596205+starbroker@users.noreply.github.com>
2026-07-30 17:59:01 +0530

    Delete scripts directory

--- scripts/fix_char.py
+++
@@ -1,8 +0,0 @@
-with open('app/src/main/java/com/example/ui/AICommandHandler.kt', 'r') as f:
-    content = f.read()
-
-import re
-content = re.sub(r'arg\.replace.*?replace.*?\)', 'arg.replace("\'", "\\\\\'").replace("\\"", "\\\\\\\"")', content)
-
-with open('app/src/main/java/com/example/ui/AICommandHandler.kt', 'w') as f:
-    f.write(content)
--- scripts/fix_intent.py
+++
@@ -1,9 +0,0 @@
-with open('app/src/main/java/com/example/ui/AICommandHandler.kt', 'r') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if 'val textToType =' in line and 'typeMatch' in lines[i-1]:
-        lines[i] = '                val textToType = typeMatch.groupValues[1].replace(' + chr(34) + '\\"' + chr(34) + ', ' + chr(34) + '\\\\\\"' + chr(34) + ').replace(' + chr(34) + "'" + chr(34) + ', ' + chr(34) + "\\\\'" + chr(34) + ')\n'
-
-with open('app/src/main/java/com/example/ui/AICommandHandler.kt', 'w') as f:
-    f.writelines(lines)
